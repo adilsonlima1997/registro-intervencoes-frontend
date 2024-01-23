e51d26c6617bb846d43c956d3049f349d290496b
Adilson Lima <addy1997lima@gmail.com>
2024-01-22 23:40:24 -0100

    22-01-2024

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Avaria } from '../modelo/Avaria';
+import { AvariaService } from '../servico/avaria.service';
 
 @Component({
   selector: 'app-principal',
@@ -9,4 +11,17 @@ export class PrincipalComponent {
 
   // variavel para visibilidade dos botões
   btnCadastro:boolean = true;
+  //JSON de avarias
+  avarias:Avaria[] = [];
+
+  constructor(private servico:AvariaService){}
+  //Método de seleção
+  selecionar():void{
+    this.servico.selecionar()
+    .subscribe(retorno => this.avarias = retorno);
+  }
+  //Método de inicialização
+  ngOnInit(){
+    this.selecionar();
+  }
 }
--- src/app/servico/avaria.service.ts
+++ src/app/servico/avaria.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Avaria } from '../modelo/Avaria';
 
 @Injectable({
   providedIn: 'root'
@@ -11,4 +13,8 @@ export class AvariaService {
 
   //aqui neste construtor o http é responsável para fazer as requisições para a nossa api
   constructor(private http:HttpClient) { }
+  //Método para selecionar todas as avarias
+  selecionar():Observable<Avaria[]>{
+    return this.http.get<Avaria[]>(this.url);
+  }
 }
